baaf2c5ed64d89597816ce6e4818ef7c24dfd6a0
vedparsekar <vedparsekar@gmail.com>
2020-08-07 18:33:14 +0530

    UI changes:contact us

--- contact us/contact_us.php
+++ contact us/contact_us.php
@@ -4,63 +4,64 @@
 	<meta charset="UTF-8">
 	<title>Contact Us</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-
-
-
    <style>
-	    body{
-	    	background-image: url(image/back2.jpg);
+	    .form-container{
+	    	background-image: url(../content/world/img/match/match-bg.jpg);
 	    	background-size:cover;
+			margin-top: 10px;
 	    }
-	    hr{
-	    	background: white;	
-      height:2px;
-	    }
-
+	   
 		.contact-form{
 			background:rgba(0,0,0, .6);
 			color:white;
 			margin-top: 80px;
 			padding: 5px;
-			box-shadow: 0px 0px 10px 3px grey;
+			
 		}
-  .detail{
-    
-    padding:60px;
-
-  }
+  		.detail{
+   			 padding:60px;
+  		}
+  		.contact1{
+	  		background-color: #e20d0d;
+			padding: 8px;
+		  text-align: center;
+  		}
+  		.bt{
+			background-color: #e20d0d;
+			color: white;
+  		}
    </style>
 
 
   </head>
 <body>
-	
-
-
+<?php 
+	include("../user/header.php");
+?>
+<div class="form-container">
+<br>
 <div class="container contact-form">
 <form  method="POST" action="process.php">
-	<h1>Contact Us</h1>
-	<hr>
-
+	<div class="contact1">
+		<h2>Contact Us</h2>
+	</div>
+	<br>
 	<div class="row">
-   
        <div class="col-md-6 detail">
        	<address>ADDRESS:- Goa University Taligao Panjim Goa.</address>
        	<p>EMAIL:- sportsgoa@gmail.com</p>
        	<p>PHONE No:-9922464496 </p>
        </div>
 
-  
        <div class="col-md-6">
-       	
          <div class="form-group">
          	<label>Name</label>
-         	<input type="text" class="form-control" id="name" name="name" placeholder="enter your name" required>
+         	<input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" required>
          </div>
 
          <div class="form-group">
          	<label>Email</label>
-         	<input type="email" class="form-control" id="email" name="email" placeholder="enter your email" required>
+         	<input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
          </div>
 
          <div class="form-group">
@@ -69,17 +70,17 @@
          </div>
 
          <div class="form-group">
-         	<button type="submit" class="btn btn-primary btn-block">Send</button>
+         	<button type="submit" class="btn btn-block bt">Send</button>
          </div>
 
-       </div>
+	   </div>
+	</div>
     </form>   
-    </div>
-
+	</div>
+	<br>
 </div>
-
-
-
-
+<?php
+	include("../footer.php");
+	?>
 </body>
 </html>
